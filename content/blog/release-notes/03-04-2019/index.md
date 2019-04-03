f9b5b20a1d9073cbd8ddce9edaaf5a99b0d83fd6
Jakob Vannucci <jakob.vannucci@protestsoftware.com.au>
2019-04-03 15:40:30 +1000

    Initial commit

---
+++ content/blog/release-notes/03-04-2019/index.md
@@ -0,0 +1,9 @@
+---
+title: Release Notes - 03/04/2019
+date: "2019-04-03T23:46:37.121Z"
+description: "Release Notes - 03/04/2019"
+---
+
+### New
+
+- Test
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import { rhythm } from "../utils/typography"
@@ -11,14 +10,12 @@ class BlogIndex extends React.Component {
     const { data } = this.props
     const siteTitle = data.site.siteMetadata.title
     const posts = data.allMarkdownRemark.edges
-
     return (
       <Layout location={this.props.location} title={siteTitle}>
         <SEO
           title="All posts"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <Bio />
         {posts.map(({ node }) => {
           const title = node.frontmatter.title || node.fields.slug
           return (
